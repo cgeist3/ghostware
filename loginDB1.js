10204043ba847987a20183dea5f3eab15ff670cd
cgeist3 <55861692+cgeist3@users.noreply.github.com>
2024-11-16 15:06:50 -0600

    Update loginDB1.js

--- loginDB1.js
+++ loginDB1.js
@@ -1,4 +1,5 @@
-console.log("Accessing Database...")
+<script>
+        console.log("Accessing Database...")
         let db;
         let playerPosition = { x: 0, y: 1, z: 0 }; // Player's initial position
         const request = indexedDB.open("GameDatabase", 1);
